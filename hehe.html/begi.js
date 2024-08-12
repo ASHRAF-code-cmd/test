fefe83d688b3e60a4d60b844db3c0c04b96d373a
ASHRAF <ashrafuddin3112002@gmail.com>
2024-08-12 23:49:17 +0530

    test website
    test website

---
+++ hehe.html/begi.js
@@ -0,0 +1,12 @@
+let userchoice = 0;
+let compchoice = 0;
+const choices=document.querySelectorAll('.x');
+choices.forEach((x)=>{
+    x.addEventListener('click',()=>{
+     console.log(x)
+    });
+
+});    
+
+
+
